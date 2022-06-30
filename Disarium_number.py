d57a62a8a1574cea659ee1cf11efc78fa79033f1
ManikantaArepalli <97425299+ManikantaArepalli@users.noreply.github.com>
2022-06-30 11:56:07 +0530

    def rev(a):     s=0     while a>0:         rem=a%10         s=(s*10)+rem         a//=10     return s a=int(input()) arev=rev(a) su=0 j=1 while arev>0:     rem=arev%10     su+=rem**j     j+=1     arev//=10 if su==a:     print("True") else:     print("False")

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,18 +1,20 @@
-n=int(input())
-c=0
-s=0
-temp=n
-while n>0:
-    r=n%10
-    n//=10
-    c+=1
-n=temp
-while n>0:
-    r=n%10
-    s=s+pow(r,c)
-    n=n//10
-    c-=1
-if s==temp:
+def rev(a):
+    s=0
+    while a>0:
+        rem=a%10
+        s=(s*10)+rem
+        a//=10
+    return s
+a=int(input())
+arev=rev(a)
+su=0
+j=1
+while arev>0:
+    rem=arev%10
+    su+=rem**j
+    j+=1
+    arev//=10
+if su==a:
     print("True")
 else:
     print("False")
