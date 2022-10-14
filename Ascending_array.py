227b030cf3bfc277a205eddf753eebaed38d465a
ManikantaArepalli <97425299+ManikantaArepalli@users.noreply.github.com>
2022-10-15 00:52:38 +0530

    n=int(input()) l=list(map(int,input().split())) for i in range(n-1):     if l[i]>=l[i+1]:         print("no")         break else:     print("yes")

--- Ascending_array.py
+++ Ascending_array.py
@@ -1,12 +1,8 @@
-n = int(input())
-arr = list(map(int,input().split()))
-c = 0
-for i in range(0,n+1):# 4
-    if i==n-1:
+n=int(input())
+l=list(map(int,input().split()))
+for i in range(n-1):
+    if l[i]>=l[i+1]:
+        print("no")
         break
-    if arr[i]<arr[i+1]:# 0 1
-        c+=1
-if c==n-1:
-    print("yes")
 else:
-    print("no")
+    print("yes")
