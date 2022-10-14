304b7a1fb45b8bab5f073425a53497f4fbbf0305
ManikantaArepalli <97425299+ManikantaArepalli@users.noreply.github.com>
2022-10-15 01:10:31 +0530

    def is_palin(n):     s=str(n)     if s[::-1]==s:         return 1     else:         return 0 n=int(input()) l=list(map(int,input().split())) c=0 for i in l:     if is_palin(i):         c+=1 print(c)

--- Count_palindromes.py
+++ Count_palindromes.py
@@ -1,14 +1,13 @@
-def rev(n):
-    rev=0
-    while n:
-        r=n%10
-        n//=10
-        rev=rev*10+r
-    return rev
+def is_palin(n):
+    s=str(n)
+    if s[::-1]==s:
+        return 1
+    else:
+        return 0
 n=int(input())
-a=list(map(int,input().split()))
-count=0
-for i in a:
-    if i==rev(i):
-        count+=1
-print(count)
+l=list(map(int,input().split()))
+c=0
+for i in l:
+    if is_palin(i):
+        c+=1
+print(c)
