edccbd559992c1afe486d40198ad1a02339f5841
ManikantaArepalli <97425299+ManikantaArepalli@users.noreply.github.com>
2022-10-15 00:44:42 +0530

    n=int(input()) arr=list(map(int,input().split())) count=0 if arr[0]>arr[1]:     for i in range(n-1):         if i%2==0 and arr[i]>arr[i+1]:             count+=1         elif i%2 and arr[i]<arr[i+1]:             count+=1     if count==n-1:         print("yes")     else:         print("no") else:     for i in range(n-1):         if i%2==0 and arr[i]<arr[i+1]:             count+=1         elif i%2 and arr[i]>arr[i+1]:             count+=1     if count==n-1:         print("yes")     else:         print("no")

--- Wave_array.py
+++ Wave_array.py
@@ -1,6 +1,6 @@
-n = int(input())
-arr = list(map(int,input().split()))
-count = 0
+n=int(input())
+arr=list(map(int,input().split()))
+count=0
 if arr[0]>arr[1]:
     for i in range(n-1):
         if i%2==0 and arr[i]>arr[i+1]:
