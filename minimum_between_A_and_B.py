0b0774baa1ed36580def3059653cc364b619acb6
ManikantaArepalli <97425299+ManikantaArepalli@users.noreply.github.com>
2022-10-14 20:11:58 +0530

    n=int(input()) l=list(map(int,input().split())) k,m=map(int,input().split()) l.sort() a=[] flag=0 for i in l:     if i>=k and i<=m:         a.append(i)         flag=1 if flag:     print(min(a)) else:     print(-1)

---
+++ minimum_between_A_and_B.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(int,input().split()))
+k,m=map(int,input().split())
+l.sort()
+a=[]
+flag=0
+for i in l:
+    if i>=k and i<=m:
+        a.append(i)
+        flag=1
+if flag:
+    print(min(a))
+else:
+    print(-1)
